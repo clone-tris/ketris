bb772557b9d8d0a1f003e4db24f904bd10a59de9
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2019-04-17 14:53:43 +0200

    change square width back to 2 pixels

--- src/com/ketris/core/screens/game/Constants.kt
+++ src/com/ketris/core/screens/game/Constants.kt
@@ -1,8 +1,6 @@
 package com.ketris.core.screens.game
 
-import java.awt.Color
-
 object Constants {
   const val SQUARE_WIDTH = 20
-  const val SQUARE_BORDER_WIDTH = 5
+  const val SQUARE_BORDER_WIDTH = 2
 }
